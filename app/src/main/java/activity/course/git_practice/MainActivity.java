cca7b5e87c9a8c42b34f8d57947752f3e302ad73
Abiezer Quiles <74214921+AQuiles69@users.noreply.github.com>
2021-12-20 17:28:41 -0500

    Testing first commit

--- app/src/main/java/activity/course/git_practice/MainActivity.java
+++ app/src/main/java/activity/course/git_practice/MainActivity.java
@@ -20,6 +20,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
+    int b = 2;
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
 
